d55f46bfca63494474468703552942f9deedeaf3
grzemal <44407007+grzemal@users.noreply.github.com>
2021-03-23 09:50:53 +0100

    change loss and penalty in SGD regressor; split in test/train data

--- task1b/main.py
+++ task1b/main.py
@@ -1,18 +1,19 @@
 import pandas as pd
 import numpy as np
 
-from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import SGDRegressor
 from sklearn.linear_model import RidgeCV
-from sklearn.linear_model import LassoCV
-from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 
 fileTrain = "train.csv"
 
 #config
-lambdas = np.linspace(0.01, 1000.0, num=10000)
+#lambdas = np.linspace(0.01, 1000.0, num=10000)
+rand_int = 42
+test_size_per = 0.15
 
+#read data
 dataFrame = pd.read_csv(fileTrain)
 y = dataFrame.y.to_numpy()
 xi = dataFrame.iloc[:, 2:].to_numpy()
@@ -27,48 +28,25 @@ def feature_description(x):
 
 feature_matrix = np.array([feature_description(xi[i]) for i in range(len(xi))])
 
-x_train, x_test, y_train, y_test = train_test_split(feature_matrix, y, test_size=0.15)
-
-#Linear regression
-reg = LinearRegression(fit_intercept=False)
-reg.fit(feature_matrix, y)
-#print(reg.score(feature_matrix, y))
-#print(reg.coef_)
-
+#x_train, x_test, y_train, y_test = train_test_split(feature_matrix, y, test_size=test_size_per, random_state=rand_int)
 
 #SDG regression
-reg = SGDRegressor(loss='huber', fit_intercept=False, random_state=42)
+reg = SGDRegressor(loss='squared_loss', penalty='l1', fit_intercept=False, random_state=rand_int)
 reg.fit(feature_matrix, y)
-#print(reg.score(feature_matrix, y))
 #print(reg.coef_)
+#print(reg.n_iter_)
 
-prediction = reg.predict(feature_matrix)
-RMSE = mean_squared_error(y, prediction) ** 0.5
-print(RMSE)
+#RMSE
+#prediction = reg.predict(x_test)
+#RMSE = mean_squared_error(y_test, prediction) ** 0.5
+#print(RMSE)
 
-#SDG regression
-reg = SGDRegressor(loss='huber', fit_intercept=False, random_state=42, learning_rate='adaptive')
-reg.fit(feature_matrix, y)
-#print(reg.score(feature_matrix, y))
-#print(reg.coef_)
 
 #Ridge regression with built-in cross-validation.
 #reg = RidgeCV(alphas = lambdas, fit_intercept=False)
 #reg.fit(feature_matrix, y)
 #print(reg.score(feature_matrix, y))
 #print(reg.alpha_)
-
-#Lasso regression with built-in cross-validation.
-#reg = LassoCV(n_alphas = 1000, alphas = None, fit_intercept=False, max_iter=10000)
-#reg.fit(feature_matrix, y)
-#print(reg.score(feature_matrix, y))
-#print(reg.alpha_)
-
-#RMSE
-prediction = reg.predict(feature_matrix)
-RMSE = mean_squared_error(y, prediction) ** 0.5
-print(RMSE)
-
 
 #save output
 np.savetxt("output.csv",reg.coef_,delimiter=",")
