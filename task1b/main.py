be0ae0cc2abe6efcc963804fde3309c0ac4ccfd7
grzemal <44407007+grzemal@users.noreply.github.com>
2021-03-26 12:44:50 +0100

    use squared_epsilon_insensitive and l2 as penalty

--- task1b/main.py
+++ task1b/main.py
@@ -1,17 +1,8 @@
 import pandas as pd
 import numpy as np
-
 from sklearn.linear_model import SGDRegressor
-from sklearn.linear_model import RidgeCV
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
 
 fileTrain = "train.csv"
-
-#config
-#lambdas = np.linspace(0.01, 1000.0, num=10000)
-rand_int = 42
-test_size_per = 0.15
 
 #read data
 dataFrame = pd.read_csv(fileTrain)
@@ -28,25 +19,9 @@ def feature_description(x):
 
 feature_matrix = np.array([feature_description(xi[i]) for i in range(len(xi))])
 
-#x_train, x_test, y_train, y_test = train_test_split(feature_matrix, y, test_size=test_size_per, random_state=rand_int)
-
 #SDG regression
-reg = SGDRegressor(loss='squared_loss', penalty='l1', fit_intercept=False, random_state=rand_int)
+reg = SGDRegressor(loss='squared_epsilon_insensitive', penalty='l2', fit_intercept=False, random_state=0)
 reg.fit(feature_matrix, y)
-#print(reg.coef_)
-#print(reg.n_iter_)
-
-#RMSE
-#prediction = reg.predict(x_test)
-#RMSE = mean_squared_error(y_test, prediction) ** 0.5
-#print(RMSE)
-
-
-#Ridge regression with built-in cross-validation.
-#reg = RidgeCV(alphas = lambdas, fit_intercept=False)
-#reg.fit(feature_matrix, y)
-#print(reg.score(feature_matrix, y))
-#print(reg.alpha_)
 
 #save output
-np.savetxt("output.csv",reg.coef_,delimiter=",")
+np.savetxt("output_sgd.csv", reg.coef_, delimiter=",")
