9cebbf4d661496d728c2ee29e48d5e8ba8475d83
grzemal <>
2021-04-15 13:52:04 +0000

    end of session 15/04

--- task2/main.py
+++ task2/main.py
@@ -1,15 +1,36 @@
 import pandas as pd
 import numpy as np
 import time
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.pipeline import make_pipeline
 
 # load the training data
-train_features = pd.read_csv('train_features.csv').\
+train_features = pd.read_csv('train_features.csv'). \
     sort_values(by=['pid', 'Time'])
-train_labels = pd.read_csv('train_labels.csv').\
+train_labels = pd.read_csv('train_labels.csv'). \
     sort_values(by=['pid'])
 
 # load the test data
 test_features = pd.read_csv('test_features.csv')
+
+# gridCV
+parameters_clf = {
+    'scoring': ['roc_auc'],
+    'random_state': [42],
+    'max_iter': [1000],
+    'max_depth': [10, 25, 50],
+    'max_bins': [20, 100, 255],
+    'max_leaf_nodes': [10, 50, 100]
+ }
+parameters_est = {
+    'scoring': ['r2'],
+    'random_state': [42],
+    'max_iter': [1000],
+    'max_depth': [10, 25, 50],
+    'max_bins': [20, 100, 255],
+    'max_leaf_nodes': [10, 50, 100]
+ }
 
 # balance imbalanced data
 
@@ -19,13 +40,13 @@ def make_features(raw_data):
     # handle missing data
     raw_data = raw_data.fillna(raw_data.groupby(['pid'], sort=False).ffill())
     raw_data = raw_data.fillna(raw_data.groupby(['pid'], sort=False).bfill())
-    raw_data = raw_data.fillna(raw_data.mean())
+    raw_data = raw_data.fillna(raw_data.median())
 
     last_meas = raw_data.groupby('pid', sort=False).last()
     median = raw_data.groupby('pid', sort=False).median()
-    
-    interquartile_range = raw_data.groupby('pid', sort=False).\
-        quantile([.25, .75]).groupby('pid', sort=False).\
+
+    interquartile_range = raw_data.groupby('pid', sort=False). \
+        quantile([.25, .75]).groupby('pid', sort=False). \
         diff().groupby('pid', sort=False).first(1)
 
     frames = [n_meas.iloc[:, 1:], last_meas.iloc[:, 1:],
@@ -33,13 +54,17 @@ def make_features(raw_data):
     features = pd.concat(frames, axis=1, join="inner")
     return features
 
+
 start = time.time()
-X = make_features(train_features)
+rnd_seed = 42
 X_test = make_features(test_features)
+X_train = make_features(train_features)
+y_train = train_labels
+
+# X_test_ = make_features(test_features)
 end = time.time()
 print(end - start)
 
-y = train_labels
 
 ##########################################################################
 #############################  Sub-task 1  ###############################
@@ -49,19 +74,30 @@ labels_sub_task1 = ["LABEL_BaseExcess", "LABEL_Fibrinogen", "LABEL_AST",
                     "LABEL_Lactate", "LABEL_TroponinI", "LABEL_SaO2",
                     "LABEL_Bilirubin_direct", "LABEL_EtCO2"]
 
+X_sub_task12 = StandardScaler().fit_transform(X_train.to_numpy())
+X_test_sub_task12 = StandardScaler().fit_transform(X_test.to_numpy())
+
 prediction_sub_task1 = np.zeros((X_test.to_numpy().shape[0],
                                  len(labels_sub_task1)))
 
 from sklearn.experimental import enable_hist_gradient_boosting
 from sklearn.ensemble import HistGradientBoostingClassifier
 
-start = time.time()
-clf = HistGradientBoostingClassifier(random_state=42)
+#clf = HistGradientBoostingClassifier(random_state=rnd_seed)
+#pipe = make_pipeline(HistGradientBoostingClassifier())
+clf = GridSearchCV(HistGradientBoostingClassifier(), parameters_clf, n_jobs=5,
+                   cv=5, scoring='roc_auc', verbose=2, refit=True)
+
 for i in range(len(labels_sub_task1)):
-    clf.fit(X.to_numpy(), y[[labels_sub_task1[i]]].to_numpy().flatten())
+    start = time.time()
+    clf.fit(X_train.to_numpy(), y_train[[labels_sub_task1[i]]].to_numpy().flatten())
     prediction_sub_task1[:, i] = clf.predict_proba(X_test.to_numpy())[:, 1]
-end = time.time()
-print(end - start)
+    #print("Score for ", labels_sub_task1[i], " :",
+    #      clf.score(X_test.to_numpy(), y_test[[labels_sub_task1[i]]].to_numpy().flatten()))
+    print("Score: ", clf.best_score_)
+    print("Best parameters :", clf.best_params_)
+    end = time.time()
+    print("Time to compute :", end - start)
 
 ##########################################################################
 #############################  Sub-task 2  ###############################
@@ -70,31 +106,46 @@ labels_sub_task2 = ["LABEL_Sepsis"]
 prediction_sub_task2 = np.zeros((X_test.to_numpy().shape[0],
                                  len(labels_sub_task2)))
 
-start = time.time()
-clf = HistGradientBoostingClassifier(random_state=42)
+#clf = HistGradientBoostingClassifier(random_state=rnd_seed)
+
 for i in range(len(labels_sub_task2)):
-    clf.fit(X.to_numpy(), y[[labels_sub_task2[i]]].to_numpy().flatten())
+    start = time.time()
+    clf.fit(X_train.to_numpy(), y_train[[labels_sub_task2[i]]].to_numpy().flatten())
     prediction_sub_task2[:, i] = clf.predict_proba(X_test.to_numpy())[:, 1]
-end = time.time()
-print(end - start)
+    #print("Score for ", labels_sub_task2[i], " :",
+    #      clf.score(X_test.to_numpy(), y_test[[labels_sub_task2[i]]].to_numpy().flatten()))
+    print("Score: ", clf.best_score_)
+    print("Best parameters :", clf.best_params_)
+    end = time.time()
+    print("Time to compute :", end - start)
 
 ##########################################################################
 #############################  Sub-task 3  ###############################
 ##########################################################################
 from sklearn.ensemble import HistGradientBoostingRegressor
+from sklearn.neural_network import MLPRegressor
 
 labels_sub_task3 = ["LABEL_RRate", "LABEL_ABPm", "LABEL_SpO2",
                     "LABEL_Heartrate"]
 
 prediction_sub_task3 = np.zeros((X_test.to_numpy().shape[0],
                                  len(labels_sub_task3)))
-start = time.time()
-est = HistGradientBoostingRegressor(random_state=42)
+
+#est = HistGradientBoostingRegressor(random_state=rnd_seed)
+#pipe = make_pipeline(HistGradientBoostingRegressor())
+est = GridSearchCV(HistGradientBoostingRegressor(), parameters_est, n_jobs=5,
+                   cv=5, scoring='r2', verbose=2, refit=True)
+
 for i in range(len(labels_sub_task3)):
-    est.fit(X.to_numpy(), y[[labels_sub_task3[i]]].to_numpy().flatten())
+    start = time.time()
+    est.fit(X_train.to_numpy(), y_train[[labels_sub_task3[i]]].to_numpy().flatten())
     prediction_sub_task3[:, i] = est.predict(X_test.to_numpy())
-end = time.time()
-print(end - start)
+    #print("Score for ", labels_sub_task3[i], " :",
+    #      est.score(X_test.to_numpy(), y_test[[labels_sub_task3[i]]].to_numpy().flatten()))
+    end = time.time()
+    print("Score: ", est.best_score_)
+    print("Best parameters :", est.best_params_)
+    print("Time to compute :", end - start)
 
 ##########################################################################
 #############################  Submission  ###############################
